f51df2b8f3f1136a64df069592a39d36eb610a1f
zx <1019033041@qq.com>
2024-03-31 17:40:43 +0800

    EP 01.

--- premake5.lua
+++ premake5.lua
@@ -1,10 +1,10 @@
 -- premake5.lua
-workspace "WalnutApp"
+workspace "RayTracingApp"
    architecture "x64"
    configurations { "Debug", "Release", "Dist" }
-   startproject "WalnutApp"
+   startproject "Project01"
 
 outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
 include "Walnut/WalnutExternal.lua"
 
-include "WalnutApp"
+include "Project01"
